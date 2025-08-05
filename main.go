fcae916a9b2e186d34b8dc7cd0779fdf864d4e11
Hunter <hamptoncode@gmail.com>
2025-08-04 22:41:04 -0500

    Add Api Key Middleware

--- main.go
+++ main.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"slices"
+	"strings"
 )
 
-const BaseURL = "https://api.theopneustos.bible"
+var ApiKeys []string
 
 func main() {
+	startup()
+	registerApiKeys()
+
 	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+	mux := http.NewServeMux()
 
-	// Get the passage and its decorated reference
-	http.HandleFunc("/{version}/reference", handleReference)
+	mux.Handle("/", fs)
+	mux.Handle("/{version}/reference", apiKeyMiddleware(http.HandlerFunc(handleReference)))
+	mux.Handle("/{version}/text", apiKeyMiddleware(http.HandlerFunc(handleText)))
 
-	// Get the passage and its hypermedia
-	http.HandleFunc("/{version}/text", handleText)
+	log.Fatal(http.ListenAndServe(":7777", mux))
+}
 
-	log.Println("Server is running on port 7777...")
-	log.Fatal(http.ListenAndServe(":7777", nil))
+func registerApiKeys() {
+	ApiKeys = append(ApiKeys, os.Getenv("API_KEY_HOMELAB_GO"))
+	ApiKeys = append(ApiKeys, os.Getenv("API_KEY_HOMELAB_GO_2"))
+}
+
+func apiKeyMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token == "" {
+			http.Error(w, "no auth", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.Split(token, " ")
+		if len(parts) != 2 {
+			http.Error(w, "invalid auth token", http.StatusUnauthorized)
+			return
+		}
+
+		splitKey := parts[1]
+		if !slices.Contains(ApiKeys, splitKey) {
+			http.Error(w, "invalid auth token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func startup() {
+	banner := `
+░██████████░██     ░██ ░██████████   ░██████   ░█████████  ░███    ░██ ░██████████ ░██     ░██   ░██████   ░██████████  ░██████     ░██████   
+    ░██    ░██     ░██ ░██          ░██   ░██  ░██     ░██ ░████   ░██ ░██         ░██     ░██  ░██   ░██      ░██     ░██   ░██   ░██   ░██  
+    ░██    ░██     ░██ ░██         ░██     ░██ ░██     ░██ ░██░██  ░██ ░██         ░██     ░██ ░██             ░██    ░██     ░██ ░██         
+    ░██    ░██████████ ░█████████  ░██     ░██ ░█████████  ░██ ░██ ░██ ░█████████  ░██     ░██  ░████████      ░██    ░██     ░██  ░████████  
+    ░██    ░██     ░██ ░██         ░██     ░██ ░██         ░██  ░██░██ ░██         ░██     ░██         ░██     ░██    ░██     ░██         ░██ 
+    ░██    ░██     ░██ ░██          ░██   ░██  ░██         ░██   ░████ ░██          ░██   ░██   ░██   ░██      ░██     ░██   ░██   ░██   ░██  
+    ░██    ░██     ░██ ░██████████   ░██████   ░██         ░██    ░███ ░██████████   ░██████     ░██████       ░██      ░██████     ░██████
+`
+	fmt.Print(banner)
+	fmt.Println("Listening on port 7777...")
 }
